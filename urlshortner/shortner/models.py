324efdf8a28fd1be492f9868634291377ebb326f
Hassan <hassansmallah@gmail.com>
2021-10-24 09:03:32 +0300

    Url model

--- urlshortner/shortner/models.py
+++ urlshortner/shortner/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Url(models.Model):
+    link = models.CharField(max_length=10000)  # some links are very long
+    uuid = models.CharField(max_length=10) # shor link id

27eea2d8c9b51fbe223dc812292452281712bb62
Hassan <hassansmallah@gmail.com>
2021-10-25 10:47:56 +0300

    Url model

--- urlshortner/shortner/models.py
+++ urlshortner/shortner/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 # Create your models here.
+
+# save user input to a model
 class Url(models.Model):
     link = models.CharField(max_length=10000)  # some links are very long
     uuid = models.CharField(max_length=10) # shor link id

df3169d9c2ad1c0a537418829f5dbcd484c74e17
Hassan <hassansmallah@gmail.com>
2021-10-30 18:51:51 +0300

    view create

--- urlshortner/shortner/views.py
+++ urlshortner/shortner/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+# get url and save it to DB
+def create(request):
+    pass

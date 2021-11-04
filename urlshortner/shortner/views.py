bb154d5e8b784f962d5fc9a30969f9d10f1e7320
Hassan <hassansmallah@gmail.com>
2021-11-04 13:57:13 +0300

    create url view

--- urlshortner/shortner/views.py
+++ urlshortner/shortner/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 import uuid
 from .models import Url
-from djano.http import HttpResponse
+from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request):
+def index(request: HttpResponse):
     return render(request, 'index.html')
 
 
 # get url and save it to DB
-def create(request):
+def create(request: HttpResponse):
     if request.method == 'POST':
         url = request.POST['link']
         # generate short id and shorten it to 5
